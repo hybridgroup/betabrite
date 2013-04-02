6edeb7f48a8f56b5ecc16ba52f1a0f5e91656dd5
deadprogrammer <ron.evans@gmail.com>
2013-04-01 23:11:56 -0700

    Update to latest Hoe, and also allow external serialport/socket for Artoo

--- Rakefile
+++ Rakefile
@@ -1,9 +1,15 @@
 require 'hoe'
 
-Hoe.spec 'betabrite' do |p|
-  developer 'Aaron Patterson', 'aaron@tenderlovemaking.com'
+Hoe.plugins.delete :rubyforge
+Hoe.plugins.delete :flog
+Hoe.plugin :minitest
+Hoe.plugin :gemspec # `gem install hoe-gemspec`
+Hoe.plugin :git     # `gem install hoe-git`
 
-  self.extra_rdoc_files  = Dir['*.rdoc']
+Hoe.spec 'hybridgroup-betabrite' do |p|
+  developer('Hybrid Group', 'betabrite@hybridgroup.com')
+
+  self.extra_rdoc_files  = FileList['*.{rdoc,markdown}']
   self.history_file      = 'CHANGELOG.rdoc'
   self.readme_file       = 'README.rdoc'
 end
--- lib/betabrite/serial.rb
+++ lib/betabrite/serial.rb
@@ -1,12 +1,17 @@
 module BetaBrite
   class Serial < Device
+    attr_reader :sp
     def initialize(serialport)
-      @serialport = serialport
+      if serialport.is_a?(String)
+        initialize_serialport serialport
+      else
+        @sp = serialport
+      end
+
       super()
     end
 
     def write_memory!
-      sp = SerialPort.new(@serialport, 9600, 8, 1, SerialPort::NONE)
       characters = 0
       memory_message.split(//).each do |chr|
         sleep 1 if characters < 6
@@ -16,13 +21,17 @@ module BetaBrite
     end
 
     def write!
-      sp = SerialPort.new(@serialport, 9600, 8, 1, SerialPort::NONE)
       characters = 0
       message.split(//).each do |chr|
         sleep 1 if characters < 6
         sp.write chr
         characters += 1
       end
+    end
+
+  private
+    def initialize_serialport(serialport)
+      @sp = SerialPort.new(serialport, 9600, 8, 1, SerialPort::NONE)
     end
   end
 end
